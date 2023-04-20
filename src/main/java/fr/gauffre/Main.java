5ea2f89fed68318d8aedf3408c71895b2ba363b8
LuKP17 <lucaspottier38@gmail.com>
2023-04-20 14:35:37 +0200

    essai modif

--- src/main/java/fr/gauffre/Main.java
+++ src/main/java/fr/gauffre/Main.java
@@ -2,6 +2,6 @@ package fr.gauffre;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hey world!");
     }
 }
