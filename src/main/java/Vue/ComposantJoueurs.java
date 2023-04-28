78b89719f7e4931bc1352abbec0ca96987a63c16
Exioloz <emilien.maillard.simon@gmail.com>
2023-04-28 17:13:04 +0200

    Update ComposantJoueurs.java

--- src/main/java/Vue/ComposantJoueurs.java
+++ src/main/java/Vue/ComposantJoueurs.java
@@ -1,31 +1,34 @@
 package Vue;
 
-import javax.imageio.ImageIO;
+import Patterns.Observateur;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-public class ComposantJoueurs extends Box {
+public class ComposantJoueurs extends Box implements Observateur {
 
 	ComposantJoueurs(int axis, CollecteurEvenements control) {
 		super(axis);
 
-		BufferedImage perso = null;
-		try {
-			perso = ImageIO.read(new File("resources/assets/personnage.png"));
-		} catch (
-				IOException exc) {
-			System.out.println("Erreur de chargement de l'icone Joueurs");
-		}
-		ImageIcon img = new ImageIcon("resources/assets/personnage.png");
-		Image image = img.getImage();
-		Image newimg = image.getScaledInstance(80, 80, Image.SCALE_SMOOTH);
-		img = new ImageIcon(newimg);
-		JLabel j1 = new JLabel(img);
-		JLabel j2 = new JLabel(img);
-		// JLabel j3 = new JLabel(img);
-		// JLabel j4 = new JLabel(img);
+		ImageIcon pb = new ImageIcon("resources/assets/penguin.png");
+		ImageIcon pv = new ImageIcon("resources/assets/penguin_vert.png");
+		ImageIcon pr = new ImageIcon("resources/assets/penguin_rouge.png");
+		ImageIcon pj = new ImageIcon("resources/assets/penguin_jaune.png");
+		Image image = pb.getImage();
+		Image newimg = image.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		pb = new ImageIcon(newimg);
+		Image image2 = pv.getImage();
+		Image newimg2 = image2.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		pv = new ImageIcon(newimg2);
+		Image image3 = pr.getImage();
+		Image newimg3 = image3.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		pr = new ImageIcon(newimg3);
+		Image image4 = pj.getImage();
+		Image newimg4 = image4.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+		pj = new ImageIcon(newimg4);
+		JLabel j1 = new JLabel(pb);
+		JLabel j2 = new JLabel(pv);
+		JLabel j3 = new JLabel(pr);
+		JLabel j4 = new JLabel(pj);
 
 		if (control.joueurCourant() == 0) {
 			JButton joueur1 = new JButton("Au tour du Joueur 1");
@@ -37,14 +40,23 @@ public class ComposantJoueurs extends Box {
 			add(joueur2);
 			add(j2);
 		}
+		else if (control.joueurCourant() == 2) {
+			JButton joueur3 = new JButton("Au tour du Joueur 3");
+			add(joueur3);
+			add(j3);
+		}
+		else if (control.joueurCourant() == 3) {
+			JButton joueur4 = new JButton("Au tour du Joueur 4");
+			add(joueur4);
+			add(j4);
+		}
+		else {
 
-		/*
-		JButton joueur3 = new JButton("Joueur 3");
-		add(joueur3);
-		add(j3);
-		JButton joueur4 = new JButton("Joueur 4");
-		add(joueur4);
-		add(j4);
-		*/
+		}
+	}
+
+	@Override
+	public void miseAJour() {
+		repaint();
 	}
 }
