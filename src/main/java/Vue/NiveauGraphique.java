ebcb2bef158eee22aaff52e1e1e388550852f22f
Exioloz <emilien.maillard.simon@gmail.com>
2023-05-09 15:31:22 +0200

    Changement de NiveauGraphique : - Ajout d'un sablier quand l'IA joue - Redécoupage du code pour enlever les segments de code identique et le rendre plus lisible - Ajout de commentaire sur les différentes conditions.

--- src/main/java/Vue/NiveauGraphique.java
+++ src/main/java/Vue/NiveauGraphique.java
@@ -10,25 +10,60 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class NiveauGraphique extends JComponent implements Observateur {
 	Jeu jeu;
-	int largeurCase, hauteurCase;
 
-	// Variables de positions
-	int x = 0;
-	int y = 0;
-	int h = 0;
-	int l = 0;
+	// Variables de positionnement pour le dessin
+	int x, y, hauteur, largeur, lignes, colonnes, largeurCase, hauteurCase;
+
+	// Formule pour calculer la distance entre 2 hexagons
+	float height;
+
+	Timer timer;
+
+	// Liste des coordonnées d'hexagones à dessiner
+	ArrayList<int[]> dessinplat;
 
 	public NiveauGraphique(Jeu j) {
 		jeu = j;
 		jeu.ajouteObservateur(this);
 	}
 
-	/*
-	 * Peindre le plateau de jeu
-	 */
+	private void grille(Graphics g, BufferedImage[] assetsPlateau) {
+		for (int i = 0; i < (lignes); i++) {
+			hauteur = Math.round((float) i * height);
+			for (int j = 0; j < (colonnes); j++) {
+				if (jeu.valeur(i, j) == 0) continue;
+				if (i % 2 == 1)
+					largeur = j * largeurCase + largeurCase / 2;
+				else
+					largeur = j * largeurCase;
+				g.drawImage(assetsPlateau[jeu.valeur(i, j)], largeur, hauteur,
+						largeurCase, hauteurCase, this);
+			}
+		}
+	}
+
+	private void feedforward(Graphics g, BufferedImage[] assetsPlateau,
+	                         ArrayList<int[]> dessinplat) {
+		for (int[] plat : dessinplat) {
+			x = plat[0];
+			y = plat[1];
+			hauteur = Math.round((float) x * height);
+			if (x % 2 == 1) {
+				largeur = y * largeurCase + largeurCase / 2;
+			} else {
+				largeur = y * largeurCase;
+			}
+			if (jeu.valeur(x, y) == 0) continue;
+			else
+				g.drawImage(assetsPlateau[jeu.valeur(x, y) + 7], largeur, hauteur,
+						largeurCase, hauteurCase, this);
+		}
+	}
+
 	@Override
 	public void paintComponent(Graphics g) {
 
@@ -36,12 +71,11 @@ public class NiveauGraphique extends JComponent implements Observateur {
 
 		g.setFont(new Font("Arial", Font.BOLD, 30));
 		/* Load Assets */
-		BufferedImage[] assetsPlateau = new BufferedImage[19];
-		BufferedImage waterBG = null;
-
+		BufferedImage[] assetsPlateau = new BufferedImage[15];
+		BufferedImage sablier = null;
 
 		try {
-			waterBG = ImageIO.read(new File("resources/assets/waterTile.png"));
+			sablier = ImageIO.read(new File("resources/assets/sablier.png"));
 			assetsPlateau[0] = ImageIO.read(new File("resources/assets/water.png"));
 			assetsPlateau[1] = ImageIO.read(new File("resources/assets/poisson1.png"));
 			assetsPlateau[2] = ImageIO.read(new File("resources/assets/poisson2.png"));
@@ -67,90 +101,58 @@ public class NiveauGraphique extends JComponent implements Observateur {
 			g.drawString("La partie est terminée", largeur() / 3, hauteur() / 2);
 		}
 
-		int lignes = jeu.hauteur();
-		int colonnes = jeu.largeur();
-		largeurCase = largeur() / colonnes;
-		hauteurCase = hauteur() / lignes;
-
 		// Rectangle d'océan (bleu) en fond
-		g.drawImage(waterBG, 0, 0, getWidth(), getHeight(), this);
+		g.setColor(new Color(51, 153, 255));
+		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.BLACK);
 
 		// DIMINUE LA TAILLE DES IMAGES IMPORTANT A PRENDRE EN COMPTE POUR LE CALCUL DES POSITIONS
 		((Graphics2D) g).scale(0.9, 1.3);
 
-		// Grille
-		// Formule pour calculer la distance entre 2 hexagons
-		float height;
+		// Variable du plateau
+		lignes = jeu.hauteur();
+		colonnes = jeu.largeur();
+		largeurCase = largeur() / colonnes;
+		hauteurCase = hauteur() / lignes;
 		height = (3f / 4f) * (float) hauteurCase;
 
-		for (int i = 0; i < (lignes); i++) {
-			h = Math.round((float) i * height);
-			for (int j = 0; j < (colonnes); j++) {
-				if (jeu.valeur(i, j) == 0) continue;
-				if (i % 2 == 1)
-					l = j * largeurCase + largeurCase / 2;
-				else
-					l = j * largeurCase;
-				g.drawImage(assetsPlateau[jeu.valeur(i, j)], l, h,
-						largeurCase, hauteurCase, this);
+		// Grille
+		grille(drawable, assetsPlateau);
+
+		// Case selectionnable durant la partie de placement des pingouins
+		if (jeu.etatCourant() == Etats.Initialisation)
+			try {
+				dessinplat = new ArrayList<>(jeu.getPingouins(1));
+				feedforward(g, assetsPlateau, dessinplat);
+			} catch (NullPointerException e) {
+				System.out.println("Erreur d'initialisation de liste");
 			}
-		}
 
-		if (jeu.etatCourant() == Etats.Initialisation) {
-			for (int[] pingouins : jeu.getPingouins(1)) {
-				x = pingouins[0];
-				y = pingouins[1];
-				h = Math.round((float) x * height);
-				if (x % 2 == 1) {
-					l = y * largeurCase + largeurCase / 2;
-				} else {
-					l = y * largeurCase;
-				}
-				if (jeu.valeur(x, y) == 0) continue;
-				else
-					g.drawImage(assetsPlateau[jeu.valeur(x, y) + 7], l, h,
-							largeurCase, hauteurCase, this);
+		// Pingouins selectionnable durant son tour
+		if (jeu.etatCourant() != Etats.Initialisation)
+			try {
+				dessinplat = new ArrayList<>(jeu.getPingouins(jeu.joueurCourant + 4));
+				feedforward(g, assetsPlateau, dessinplat);
+			} catch (NullPointerException e) {
+				System.out.println("Erreur d'initialisation de liste");
 			}
-		}
 
-
-		if (jeu.etatCourant() != Etats.Initialisation) {
-			for (int[] pingouins : jeu.getPingouins(jeu.joueurCourant + 4)) {
-				x = pingouins[0];
-				y = pingouins[1];
-				h = Math.round((float) x * height);
-				if (x % 2 == 1) {
-					l = y * largeurCase + largeurCase / 2;
-				} else {
-					l = y * largeurCase;
-				}
-				if (jeu.valeur(x, y) == 0) continue;
-				else
-					g.drawImage(assetsPlateau[jeu.valeur(x, y) + 7], l, h,
-							largeurCase, hauteurCase, this);
+		// Case Selectionnable après sélection d'un pingouins
+		if (jeu.etatCourant() == Etats.Deplacement)
+			try {
+				dessinplat = new ArrayList<>(jeu.hexAccess);
+				feedforward(g, assetsPlateau, dessinplat);
+			} catch (NullPointerException e) {
+				System.out.println("Erreur d'initialisation de liste");
 			}
-		}
 
-		// Cases selectionnable (feedforward)
-		try {
-			if (jeu.etatCourant() == Etats.Deplacement)
-				for (int[] access : jeu.hexAccess) {
-					x = access[0];
-					y = access[1];
-					h = Math.round((float) x * height);
-					if (x % 2 == 1) {
-						l = y * largeurCase + largeurCase / 2;
-					} else {
-						l = y * largeurCase;
-					}
-					if (jeu.valeur(x, y) == 0) continue;
-					else
-						g.drawImage(assetsPlateau[jeu.valeur(x, y) + 7], l, h,
-								largeurCase, hauteurCase, this);
-				}
-		} catch (NullPointerException e) {
-			System.out.println("Erreur d'initialisation de liste");
-		}
+		// Dessine un sablier si l'IA joue
+		if (jeu.joueurs[jeu.joueurCourant].estIA)
+			g.drawImage(sablier, 0, 0, largeurCase, hauteurCase, this);
+
+		// TODO : Dessine l'animation de déplacement d'un pingouins
+		if (jeu.etatCourant() != Etats.Initialisation)
+			;
 	}
 
 	int largeur() {
