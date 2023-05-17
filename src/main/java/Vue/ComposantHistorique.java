97b2ef29c7d3c4c0735a5b09a4edae3dff4c0be4
Emilien MAILLARD-SIMON <44348621+Exioloz@users.noreply.github.com>
2023-05-17 15:58:12 +0200

    Update ComposantHistorique.java

--- src/main/java/Vue/ComposantHistorique.java
+++ src/main/java/Vue/ComposantHistorique.java
@@ -6,28 +6,32 @@ import javax.swing.*;
 
 public class ComposantHistorique extends Box implements Observateur {
 
-	ComposantHistorique(CollecteurEvenements control) {
+	JButton annuler;
+	JButton refaire;
+	CollecteurEvenements control;
+	ComposantHistorique(CollecteurEvenements c) {
 		super(BoxLayout.X_AXIS);
-		JButton annuler = new JButton("Annuler");
+		control = c;
+		annuler = new JButton("Annuler");
 		annuler.addActionListener(new AdaptateurAnnuler(control));
 		add(annuler);
-		JButton refaire = new JButton("Refaire");
+		refaire = new JButton("Refaire");
 		refaire.addActionListener(new AdaptateurRefaire(control));
 		add(refaire);
 
 		// Impossible de cliquer les boutons avant qu'un move soit joué
-		if(control.etatPla())
-			annuler.setEnabled(true);
-		else annuler.setEnabled(false);
-		if(control.etatDep())
-			refaire.setEnabled(true);
-		else annuler.setEnabled(false);
+		annuler.setEnabled(false);
+		refaire.setEnabled(false);
 
 	}
 
 	@Override
 	public void miseAJour() {
-		revalidate();
-		repaint();
+		if(control.etatSel()) {
+			annuler.setEnabled(true);
+		}
+		if(control.etatDep()) {
+			refaire.setEnabled(true);
+		}
 	}
 }
