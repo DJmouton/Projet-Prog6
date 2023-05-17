46b3b756912e249a1105c796fee9be9bdfd38e40
Emilien MAILLARD-SIMON <44348621+Exioloz@users.noreply.github.com>
2023-05-17 15:41:40 +0200

    Debut de la modification pour désactiver les boutons refaire et annuler
    Désactiver les boutons refaire et annuler quand ils ne font rien

--- src/main/java/Vue/ComposantHistorique.java
+++ src/main/java/Vue/ComposantHistorique.java
@@ -1,8 +1,10 @@
 package Vue;
 
+import Patterns.Observateur;
+
 import javax.swing.*;
 
-public class ComposantHistorique extends Box {
+public class ComposantHistorique extends Box implements Observateur {
 
 	ComposantHistorique(CollecteurEvenements control) {
 		super(BoxLayout.X_AXIS);
@@ -12,5 +14,20 @@ public class ComposantHistorique extends Box {
 		JButton refaire = new JButton("Refaire");
 		refaire.addActionListener(new AdaptateurRefaire(control));
 		add(refaire);
+
+		// Impossible de cliquer les boutons avant qu'un move soit joué
+		if(control.etatPla())
+			annuler.setEnabled(true);
+		else annuler.setEnabled(false);
+		if(control.etatDep())
+			refaire.setEnabled(true);
+		else annuler.setEnabled(false);
+
+	}
+
+	@Override
+	public void miseAJour() {
+		revalidate();
+		repaint();
 	}
 }
