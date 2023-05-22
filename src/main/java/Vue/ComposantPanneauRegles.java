4010d9bebfcf405382b5d1e4fc3d02c868f1cca8
LuKP17 <lucaspottier38@gmail.com>
2023-05-22 16:07:00 +0200

    Modif explication règles

--- src/main/java/Vue/ComposantPanneauRegles.java
+++ src/main/java/Vue/ComposantPanneauRegles.java
@@ -5,39 +5,29 @@ import javax.swing.*;
 public class ComposantPanneauRegles extends JPanel {
 
     ComposantPanneauRegles(){
-        add(new JLabel("<html>Règles :<br>" +
-                "Ce joue de 2 à 4 joueurs<br>" +
-                "- Chaque joueurs possèdent : <br>" +
-                "4 pinougins chacun pour 2 joueurs<br>" +
-                "3 pingouins chacun pour 3 joueurs<br>" +
-                "2 pingouins chacun pour 4 joueurs<br>" +
-                "Le plateau est composé : <br>" +
-                "60 iceberg, chacun contenant un certain nombre de poisson :<br>" +
-                "- 30 icebergs de 1 poisson<br>" +
-                "- 20 icebergs de 2 poissons<br>" +
-                "- 10 icebergs de 3 poissons<br>" +
+        add(new JLabel("<html>2 à 4 joueurs<br>" +
+                "2 joueurs = 4 pingouins chacun<br>" +
+                "3 joueurs = 3 pingouins chacun<br>" +
+                "4 joueurs = 2 pingouins chacun<br>" +
                 "<br>" +
-                "- Objectif de la partie :<br>" +
-                "Pour gagner il faut être le joueur avec le plus de poissons à la fin de la partie.<br>" +
-                "Si des joueurs ont le même nombre de poissons alors le joueur avec le plus d'icebergs retiré du jeu gagne.<br>" +
-                "En cas d'égalité de poisson et d'iceberg il y a alors égalité entre ces joueurs.<br>" +
+                "- 1ère phase : Placement<br>" +
+                "Chaque joueur pose à tour de rôle un pingouin sur un iceberg à 1 poisson<br>" +
+                "jusqu'à ce que tous les pingouins soient posés.<br>" +
                 "<br>" +
-                "- Fin de partie : <br>" +
-                "Lorsqu'un pingouin n'a plus aucun mouvement disponible il est retiré du jeu.<br>" +
-                "La partie se termine lorsque tous les pingouins ont été retiré du jeu.<br>" +
-                "Si un joueur n'a plus de pingouin il passe son tour.<br>" +
+                "- 2ème phase : Déplacement<br>" +
+                "Chaque joueur déplace à tour de rôle un de ses pingouins sur un iceberg.<br>" +
+                "Cliquer sur un pingouin entouré pour le sélectionner.<br>" +
+                "Ensuite cliquer sur un iceberg entouré pour le déplacer à cet endroit.<br>" +
+                "Le joueur récupère alors les poissons se trouvant sur cet iceberg<br>" +
+                "ainsi que l'iceberg où se trouvait le pingouin avant son déplacement.<br>" +
                 "<br>" +
-                "- Début de partie :<br>" +
-                "A tour de rôle chaque joueur pose un pingouin sur un iceberg<br>" +
-                "jusqu'à ce que tous leurs pingouins soient posé.<br>" +
-                "Un pingouin ne peut être posé uniquement sur un iceberg contenant 1 poisson.<br>" +
+                "- Fin de partie<br>" +
+                "Un pingouin est retiré du jeu si il ne peut plus se déplacer.<br>" +
+                "Un joueur est éliminé si il a perdu tous ses pingouins.<br>" +
+                "La partie se termine lorsqu'il n'y a plus aucun pingouin en jeu.<br>" +
                 "<br>" +
-                "- Déroulement d'un tour :<br>" +
-                "A chaque tour un joueur peut choisir un pingouin et peut le déplacer sur une case en ligne depuis sa position,<br>" +
-                "or, un pingouin ne peut pas traverser un vide ou un iceberg où un autre pingouin est déjà posé.<br>" +
-                "L'iceberg que vient de quitter le pingouin est alors retirée du jeu et<br>" +
-                "récupérer par le joueur qui vient de bouger son pingouin.<br>" +
-                "Lorsqu'un pingouin arrive sur un iceberg il mange les poissons qui s'y trouve," +
-                "<br>ces poissons s'ajoutent au score total du joueur à qui appartient ce pingouin.</html>"));
+                "- Objectif : être le joueur ayant mangé le plus de poissons !<br>" +
+                "En cas d'égalité, on départage selon le nombre d'icebergs récupérés.<br>" +
+                "Si plusieurs joueurs ont le même nombre de poissons et d'icebergs, il y a égalité parfaite.</html>"));
     }
 }
